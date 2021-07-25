2cc25980975940ed507fbe3a69e2502544b1cdd3
JS <jsncortes@gmail.com>
2021-07-24 21:15:56 -0400

    integrado validacione

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -45,40 +45,136 @@ function calculateDV(rut) {
 }
 //funcion validar campos vacíos
 function fieldForm(field) {
-    if (field === "") {
-        alert("Por favor completa todos los campos");
+    if (!(field === "")) {
+        return true;
     } else {
-        return field;
+        return false;
     }
 }
+
 //funcion limpiar formulario
 function cleanForm(id) {
     document.getElementById(id).reset();
 }
-//funcion validar email con expresión regular
-function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+
 //funcion validar fecha
 
 //funcion valores negativos y decimales
-function formatNumber(number){
-    if (number < 0){
+function formatNumber(number) {
+    if (number < 0) {
         number = number * -1;
         return number;
-    }else{
+    } else {
         return number;
     }
 }
 
-//funcion 
+//funcion limpiar formulario
+function clean(id) {
+    document.getElementById(id).reset();
+}
+//funcion validaciones
+function validaciones(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (fieldForm(array[i].rut)) {
+            if (rutEsValido(array[i].rut)) {
+                if (!(array[i].region == "0")) {
+                    return true;
+                } else {
+                    alert("debe seleccionar región");
+                }
+            } else {
+                alert("rut no es valido");
+            }
+        } else {
+            alert("rut no puede estar vacío");
+        }
+    }
+}
 
+// *** Funciones para llenado de tabla detalle de ventas ***
 
-//validando las funciones
-var email = "test@gmail.com";
-var rut = "11111111-1";
-var number = -3;
-console.log("función validar email:",email,validateEmail(email));
-console.log("función validar rut:",rut,rutEsValido(rut));
-console.log(formatNumber(number));
+// Declaracion de constantes y variables
+const inputRut = document.getElementById("rut");
+const inputNombre = document.getElementById("nombre");
+const inputFecha = document.getElementById("fecha");
+const inputRegion = document.getElementById("region");
+const inputTotal = document.getElementById("total");
+const div = document.getElementById("detalle");
+const btn = document.getElementById("btnAgregar");
+let detalle = [];
+btn.addEventListener("click", crearVenta);
+
+//Creacion de la clase Venta
+function Venta() {
+    this.rut = "";
+    this.nombre = "";
+    this.fecha = "";
+    this.region = "";
+    this.total = 0;
+}
+
+// Funcion para creacion de objetos
+function crearVenta() {
+    let venta = new Venta();
+
+    venta.rut = inputRut.value;
+    venta.nombre = inputNombre.value;
+    venta.fecha = inputFecha.value;
+    venta.region = inputRegion.value;
+    venta.total = inputTotal.value;
+    detalle.push(venta);
+    if (validaciones(detalle)) {
+        imprimirVentas();
+        sumarVentas();
+    }
+}
+
+// Limpiar campos del formulario
+function limpiar() {
+    inputRut.value = "";
+    inputNombre.value = "";
+    inputFecha.value = "";
+    inputRegion.value = "0";
+    inputTotal.value = "0";
+}
+
+//Agregar venta a la tabla Detalle de venta
+function imprimirVentas() {
+    const cardVentas = document.getElementById("cardVentas");
+    const cardTotales = document.getElementById("cardTotales");
+    let html = "";
+    let totalVenta = 0;
+    //if (validaciones(detalle)) {
+    for (let i = 0; i < detalle.length; i++) {
+        totalVenta += detalle[i].total;
+        html += "<tr><td>" + (i + 1) + "</td>"
+        html += "<td>" + detalle[i].nombre + "</td>";
+        html += "<td>" + detalle[i].rut + "</td>";
+        html += "<td>" + detalle[i].fecha + "</td>";
+        html += "<td>" + detalle[i].region + "</td>";
+    }
+
+    div.innerHTML = html;
+
+    //Grabar contador de ventas en un parrafo
+    pVentas = "<p class='card-text'>" + contarVentas() + "</p>";
+    pTotales = "<p class='card-text'>" + sumarVentas() + "</p>";
+    cardVentas.innerHTML = pVentas;
+    cardTotales.innerHTML = pTotales;
+    limpiar();
+    //}
+}
+//Devuelve el total de ventas
+function contarVentas() {
+    return detalle.length;
+}
+
+//Devuelve la suma de las ventas
+function sumarVentas() {
+    let sumaVenta = 0;
+    for (let i = 0; i < detalle.length; i++) {
+        sumaVenta += parseInt(detalle[i].total);
+    }
+    return sumaVenta;
+}
